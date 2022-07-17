95bcb7f840936b24e0222a7e991def99613f6629
F1L1P <mlodzik.filip@wp.pl>
2022-07-17 14:09:43 +0200

    Cleaning up

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
-from cvzone.HandTrackingModule import HandDetector
-import json
-import cvzone
+from HandDetector import HandDetector
 import socket
+
 cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture('http://172.16.0.145:4747/mjpegfeed')
 
 scrnwidth = 640
 scrnheight = 480
@@ -25,6 +25,8 @@ def fdfc(f,W,p1,p2):
 
 while True:
     success,img  =  cap.read()
+
+    #img2 = img.copy()
 
     data=""
 
@@ -49,6 +51,7 @@ while True:
 
             Z_real = d
 
+
             cv2.putText(img, f'X{round(X_real)}cm', (lx, ly - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
             cv2.putText(img, f'Y{round(Y_real)}cm', (lx, ly - 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
             cv2.putText(img, f'Z{round(Z_real)}cm', (lx, ly - 60), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
@@ -59,11 +62,16 @@ while True:
 
 
             data+=f"{hand['type']}:{[X_real,Y_real,Z_real]}:{lmArray};"
+            #data+=f"{hand['type']}:{[round(X_real),round(Y_real),round(Z_real)]};"
+
         
 
         print(round(X_real),round(Y_real),round(Z_real))
 
         socket.sendto(str.encode(data),socketAdressPort)
 
+    
+
     cv2.imshow("img",img)
+    #cv2.imshow("img2",img2)
     cv2.waitKey(1)
